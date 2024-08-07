948ce70770c47f753fc3ea91907ffafb0640a732
杨旸 <yyned2501@vip.qq.com>
2024-08-07 09:05:29 +0800

    移除server相关代码

--- server/app/views/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask
-
-def register_blueprint(app: Flask):
-    from .index import index
--- server/main.py
+++
@@ -1,6 +0,0 @@
-from app.init import init_app
-
-app = init_app()
-
-if __name__ == "__main__":
-    app.run("0.0.0.0", 5000, debug=True)
