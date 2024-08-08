a5157a1ced106d930b45b6e096e406d4436f3508
杨旸 <yyned2501@vip.qq.com>
2024-08-08 09:16:43 +0800

    添加自动添加config字段

---
+++ client/setconfig.py
@@ -0,0 +1,19 @@
+import config_example
+import config
+
+
+def mk_write(k, v):
+    if isinstance(v, str):
+        return f"{k} = '{v}'\n"
+    else:
+        return f"{k} = {v}\n"
+
+
+if __name__ == "__main__":
+    with open("config.py", "w") as f:
+        for k, v in vars(config_example).items():
+            if not k.startswith("__"):
+                if k in dir(config):
+                    f.write(mk_write(k, getattr(config, k)))
+                else:
+                    f.write(mk_write(k, v))
